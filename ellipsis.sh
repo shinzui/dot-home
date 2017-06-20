3622221592ed312fddb5102fa6691795870e846f
Nadeem Bitar <nadeem@gmail.com>
2017-06-19 21:25:08 -0700

    Remove optional brew packages

--- ellipsis.sh
+++ ellipsis.sh
@@ -22,13 +22,13 @@ pkg.install() {
 
 install_brew_packages() {
   brew update
-  brew install fasd shellcheck fzf jq zsh bash bash-completion exiftool ssh-copy-id the_silver_searcher tmux imagemagick git git-extras \
+  brew install fasd shellcheck fzf jq zsh ssh-copy-id the_silver_searcher tmux git git-extras \
     reattach-to-user-namespace shellcheck md5sha1sum
 
   #install fzf's shell extensions
   /usr/local/opt/fzf/install
 
-  brew install postgresql rethinkdb redis ruby-build node nsq
+  brew install postgresql redis node 
 }
 
 copy_fonts() {
