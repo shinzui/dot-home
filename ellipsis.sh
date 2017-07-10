ea4a543b379cd3e535a677c600eaccfc594a7dfb
Nadeem Bitar <nadeem@gmail.com>
2017-07-10 07:20:04 -0700

    :package: Add gpg homebrew package

--- ellipsis.sh
+++ ellipsis.sh
@@ -23,7 +23,7 @@ pkg.install() {
 install_brew_packages() {
   brew update
   brew install fasd shellcheck fzf jq zsh ssh-copy-id the_silver_searcher tmux git git-extras \
-    reattach-to-user-namespace shellcheck md5sha1sum mas
+    reattach-to-user-namespace shellcheck md5sha1sum mas gpg
 
   #install fzf's shell extensions
   /usr/local/opt/fzf/install
